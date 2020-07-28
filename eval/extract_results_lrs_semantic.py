6ba6c1cc6b3582f10e23b7627e0b2b754c644bc9
Sherif Abdelkarim <sherif.abdelkarim91@gmail.com>
2020-07-27 19:46:54 -0500

    Minor changes

--- eval/extract_results_lrs_semantic.py
+++ eval/extract_results_lrs_semantic.py
@@ -12,10 +12,11 @@ model_names = sorted(model_names)
 tembedding_path= root + 'eval/eval_files/TEmbeddings/large_scale/semantic/'
 
 for model_name in model_names:
+    results = []
     if '8tasks_semantic' in model_name:
         print(model_name)
         for b in range(1, 9):
-            X_embedding=scipy.io.loadmat(root + '/outputs/CV_feat/' + model_name + '/B' + str(b) + 'XEmbeddings.mat')
+            X_embedding=scipy.io.loadmat(root + 'outputs/CV_feat/' + model_name + '/B' + str(b) + 'XEmbeddings.mat')
             X_embedding=X_embedding['XE']
             T_embedding=scipy.io.loadmat(tembedding_path + 'TEmbedding_rnd_B' + str(b) + '_test.mat')
             T_embedding=T_embedding['B_T']
@@ -24,6 +25,9 @@ for model_name in model_names:
             res_name= root + 'results/lrs_results/semantic/' + model_name + '_batch' + str(b) + '_m1_nodup.mat'
             Result_exact=GetResults_exact_metric1(X_embedding,T_embedding,TestData,T_embedding,flag_nodup=True)
             # pprint(Result_exact['mAP'])
-            print('T{}:'.format(b), '{:.2f}%'.format(Result_exact['KnowledgeMeanDetRatio_K5'] * 100.))
+            results.append('{:.2f}%'.format(Result_exact['KnowledgeMeanDetRatio_K5'] * 100.))
+            # print('T{}:'.format(b), '{:.2f}%'.format(Result_exact['KnowledgeMeanDetRatio_K5'] * 100.))
             # exit()
             # save(res_name,'Result_exact') # Todo: save as json
+    print('T1, T2, T3, T4, T5, T6, T7, T8, mean:')
+    print(*results, sep'\t')
--- eval/extract_results_mds_random.py
+++ eval/extract_results_mds_random.py
@@ -12,6 +12,7 @@ model_names = sorted(model_names)
 tembedding_path= root + 'eval/eval_files/TEmbeddings/mid_scale/random/Temb_MD_random/'
 
 for model_name in model_names:
+    results = []
     if '4tasks_random' in model_name:
         print(model_name)
         for b in range(1, 5):
@@ -24,6 +25,9 @@ for model_name in model_names:
             res_name= root + '/eval/results/mds_results/random/' + model_name + '_batch' + str(b) + '_m1_nodup.mat'
             Result_exact=GetResults_exact_metric1(X_embedding,T_embedding,TestData,T_embedding,flag_nodup=True)
             # pprint(Result_exact['mAP'])
-            print('T{}:'.format(b), '{:.2f}%'.format(Result_exact['KnowledgeMeanDetRatio_K5'] * 100.))
+            results.append('{:.2f}%'.format(Result_exact['KnowledgeMeanDetRatio_K5'] * 100.))
+            # print('T{}:'.format(b), '{:.2f}%'.format(Result_exact['KnowledgeMeanDetRatio_K5'] * 100.))
             # exit()
             # save(res_name,'Result_exact') # Todo: save as json
+    print('T1, T2, T3, T4, mean:')
+    print(*results, sep'\t')
